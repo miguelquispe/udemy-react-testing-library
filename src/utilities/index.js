38055a3e95658dd4d35d2186d01aee139705a595
Miguel Quispe <miguel.quispe@globant.com>
2022-10-28 12:17:55 -0500

    Test and code context for scoops subtotal

---
+++ src/utilities/index.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * @param {number} currency
+ * @returns {string} number formatted as currency.
+ *
+ * @example
+ *    formatCurrency(0)
+ *    // => $0.00
+ *
+ * * @example
+ *    formatCurrency(1.5)
+ *    // => $1.50
+ *
+ */
+export default function formatCurrency(currency) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(currency);
+}
